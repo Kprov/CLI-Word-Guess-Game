b571ff3035702991a456fd3f5af3a45d918b14e0
Mr_Cheese <kevinprovost776@gmail.com>
2019-02-28 17:45:01 -0600

    test

--- word.js
+++ word.js
@@ -1,15 +1,32 @@
+var Letter = require("/letter.js");
+
 function Word(letterArray){
     this.letterArray = letterArray;
+    this.testWord = [];
+    this.makeWord = function() {
+        for (var i=0;i<wordsArray.length;i++){
+            var let = new Letter(wordArray[i]);
+            this.testWord.push(let);
+        }
+    }
+    this.showWord = function() {
+        var wordDisplay = [];
+        for (var i=0;i<this.testWord.length;i++){
+            wordDisplay.push(this.testWord[i].displayLet());
+        }
+        return wordDisplay.join(" ");
+    }
+    this.checkGuess = function(myGuess) {
+        for (var i=0;i<this.testWord.length;i++){
+            this.testWord[i].guess(myGuess);
+        }
+    }
 }
-var citiesArray = [];
-var wordsArray = ["chicago", "detroit", "dallas", "seattle", "oakland", "missoula", "springfield", "atlanta", "miami", "houston","phoenix"];
-for (var i=0;i<wordsArray.length;i++){
-    
-    
-citiesArray.push(wordsArray[i]);
-test = citiesArray[i].split(" ");
-console.log(test);
+
+module.exports = Word;
 
 
 
-}
+
+
+
